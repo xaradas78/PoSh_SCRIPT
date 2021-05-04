5cf5faf84e6441adb2b538006a958b2fed86fd8f
xaradas78 <luca.fidanza@gmail.com>
2021-05-04 23:00:08 +0200

    c

--- SCRIPT/IpResetOnDhcpFail/IpResetOnDhcpFail.ps1
+++ SCRIPT/IpResetOnDhcpFail/IpResetOnDhcpFail.ps1
@@ -16,8 +16,11 @@ function Log2File([String]$LogFile,[String]$Message,[ValidateSet('Info','Warning
 [string]$ApipaAddress = "169.254"
 [string]$BaseDirectory = "C:\SWSIA\logs"
 [string]$LogFile = "IpResetOnDhcpFail.log"
-[string]$LAP = $BaseDirectory + "\" + $LogFile
-[int]$maxLogFileSizeKB = 15000
+[string]$LogConf = "IpResetOnDhcpFail_Conf.log"
+[string]$LF = $BaseDirectory + "\" + $LogFile
+[string]$LC = $BaseDirectory + "\" + $LogConf
+[int]$maxLogFileSizeKB = 1500
+[int]$maxLogConfSizeKB = 1500
 
 
 # Logica
@@ -26,57 +29,57 @@ if(!(Test-Path -path $BaseDirectory))
     New-Item -ItemType directory -Path $BaseDirectory
 }
 
-[int]$logFileSize = [math]::Round((Get-Item $LAP).length/1024)
+[int]$logFileSize = [math]::Round((Get-Item $LF).length/1024)
+[int]$logConfSize = [math]::Round((Get-Item $LC).length/1024)
 
-if ($logFileSize -gt $maxLogFileSizeKB)
-{
-    Remove-Item -Path $LAP -Force
-}
+if ($logFileSize -gt $maxLogFileSizeKB) { Remove-Item -Path $LF -Force }
+if ($logConfSize -gt $maxLogConfSizeKB) { Remove-Item -Path $LC -Force }
 
-Log2File -LogFile $LAP -Message "Avvio" -Type "Info"
+
+Log2File -LogFile $LF -Message "Avvio" -Type "Info"
 
 $currentConf = Get-NetIPConfiguration -Detailed | Out-String -Width 200
-Log2File -LogFile $LAP -Message "Configurazione corrente" -Type "Info"
-Log2File -LogFile $LAP -Message $currentConf -Type "Info"
+Log2File -LogFile $LF -Message "Export Configurazione corrente" -Type "Info"
+Log2File -LogFile $LC -Message $currentConf -Type "Info"
 $currentConf = ipconfig /all | Out-String -Width 200
-Log2File -LogFile $LAP -Message $currentConf -Type "Info"
+Log2File -LogFile $LC -Message $currentConf -Type "Info"
 
 $ipAddress = Get-NetAdapter -Physical | Where-Object {$_.Status -eq "Up"} | Get-NetIPAddress -AddressFamily IPv4 | Select-Object IPAddress
 
-[string]$ip = $ipAddress.IPAddress.Substring(0,7)
+Log2File -LogFile $LF -Message "Valore ip: $($ipAddress.IPAddress)" -Type "Info"
 
-Log2File -LogFile $LAP -Message "Valore ip: $ip" -Type "Info"
+[string]$ip = $ipAddress.IPAddress.Substring(0,7)
 
 #$ip = "169.254"
 
 if ($ip -eq $ApipaAddress)
 {
-    Log2File -LogFile $LAP -Message "Attenzione IP in APIPA" -Type "Error"
+    Log2File -LogFile $LF -Message "Attenzione IP in APIPA" -Type "Error"
 
-    Log2File -LogFile $LAP -Message "Avvio azione correttiva: netsh winsock reset" -Type "Warning"
+    Log2File -LogFile $LF -Message "Avvio azione correttiva: netsh winsock reset" -Type "Warning"
     netsh winsock reset
 
-    Log2File -LogFile $LAP -Message "Avvio azione correttiva: netsh int ip reset" -Type "Warning"
+    Log2File -LogFile $LF -Message "Avvio azione correttiva: netsh int ip reset" -Type "Warning"
     netsh int ip reset
 
-    Log2File -LogFile $LAP -Message "Avvio azione correttiva: netsh advfirewall reset" -Type "Warning"
+    Log2File -LogFile $LF -Message "Avvio azione correttiva: netsh advfirewall reset" -Type "Warning"
     netsh advfirewall reset
 
-    Log2File -LogFile $LAP -Message "Avvio azione correttiva: netsh winhttp>reset proxy" -Type "Warning"
+    Log2File -LogFile $LF -Message "Avvio azione correttiva: netsh winhttp>reset proxy" -Type "Warning"
     netsh winhttp>reset proxy
 
-    Log2File -LogFile $LAP -Message "Avvio azione correttiva: ipconfig /release" -Type "Warning"
+    Log2File -LogFile $LF -Message "Avvio azione correttiva: ipconfig /release" -Type "Warning"
     ipconfig /release
 
-    Log2File -LogFile $LAP -Message "Avvio azione correttiva: ipconfig /renew" -Type "Warning"
+    Log2File -LogFile $LF -Message "Avvio azione correttiva: ipconfig /renew" -Type "Warning"
     ipconfig /renew
 
-    Log2File -LogFile $LAP -Message "Avvio azione correttiva: ipconfig /flushdns" -Type "Warning"
+    Log2File -LogFile $LF -Message "Avvio azione correttiva: ipconfig /flushdns" -Type "Warning"
     ipconfig /flushdns
 }
 else
 {
-    Log2File -LogFile $LAP -Message "DHCP sembra essere operativo" -Type "Info"   
+    Log2File -LogFile $LF -Message "DHCP sembra essere operativo" -Type "Info"   
 }
 
-Log2File -LogFile $LAP -Message "Fine" -Type "Info"
+Log2File -LogFile $LF -Message "Fine" -Type "Info"
